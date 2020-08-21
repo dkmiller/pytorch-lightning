4400a3be631886464d599a0be6ab9e47e4aedf5c
Dan Miller <danmill@microsoft.com>
2020-08-21 12:02:57 -0700

    moving dev to linux

--- pytorch_lightning/loggers/azureml.py
+++ pytorch_lightning/loggers/azureml.py
@@ -1,12 +1,33 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Azure Machine Learning
+----------------------
+"""
+
 from argparse import Namespace
-from typing import Optional, Dict, Union, Any
+from typing import BinaryIO, Optional, Dict, Union, Any
 
 try:
     from azureml.core import Run as AzureMlRun
     from azureml.core.run import _OfflineRun as AzureMlOfflineRun
+    import matplotlib.pyplot as PyPlot
 except ImportError:  # pragma: no-cover
     AzureMlOfflineRun = None
     AzureMlRun = None
+    PyPlot = None
     _AZURE_ML_AVAILABLE = False
 else:
     _AZURE_ML_AVAILABLE = True
@@ -23,13 +44,25 @@ from pytorch_lightning.utilities import rank_zero_only
 
 class AzureMlLogger(LightningLoggerBase):
     r"""
-    TODO: docs.
+    Log using `Azure Machine Learning <https://docs.microsoft.com/en-us/azure/machine-learning/>`_. Install it with pip:
+
+    .. code-block:: bash
+
+        pip install azureml-sdk
+
+    Azure Machine Learning requires ???.
+
+    Args:
+        run: ?
     """
+
     def __init__(self, run: Optional[AzureMlRun] = None):
-        
+
         if not _AZURE_ML_AVAILABLE:
-            raise ImportError('You want to use `azureml-defaults` logger which is not installed yet,'
-                              ' install it with `pip install azureml-defaults`.')
+            raise ImportError(
+                "You want to use `azureml-sdk` logger which is not installed yet,"
+                " install it with `pip install azureml-sdk`."
+            )
         super().__init__()
 
         if run is None:
@@ -40,6 +73,15 @@ class AzureMlLogger(LightningLoggerBase):
     @property
     @rank_zero_experiment
     def experiment(self) -> AzureMlRun:
+        r"""
+        Actual Azure Machine Learning `Run` object. To use Azure ML features in
+        your :class:`~pytorch_lightning.core.lightning.LightningModule` do the following.
+
+        Example::
+
+            self.logger.experiment.some_azure_ml_function()
+
+        """
         return self._experiment
 
     @rank_zero_only
@@ -50,12 +92,8 @@ class AzureMlLogger(LightningLoggerBase):
             self.experiment.tag(name, value)
 
     @rank_zero_only
-    def log_metrics(
-            self,
-            metrics: Dict[str, Union[torch.Tensor, float]],
-            step: Optional[int] = None
-    ) -> None:
-        assert rank_zero_only.rank == 0, 'experiment tried to log from global_rank != 0'
+    def log_metrics(self, metrics: Dict[str, Union[torch.Tensor, float]], step: Optional[int] = None) -> None:
+        assert rank_zero_only.rank == 0, "experiment tried to log from global_rank != 0"
 
         # Azure ML expects metrics to be a dictionary of detached tensors on CPU
         for key, val in metrics.items():
@@ -64,31 +102,53 @@ class AzureMlLogger(LightningLoggerBase):
             self.experiment.log(key, val)
 
     @rank_zero_only
+    def log_image(self, name: str, path: Union[str, BinaryIO] = None, plot: PyPlot = None, description: str = ""):
+        """
+        Log an image metric to the run record.
+        """
+        self.experiment.log_image(name, path, plot, description)
+
+    @rank_zero_only
+    def log_list(self, name: str, value: list, description: str = ""):
+        """
+        Log a list of metric values to the run with the given name.
+        """
+        self.experiment.log_list(name, value, description)
+
+    @rank_zero_only
+    def log_table(self, name: str, value: dict, description: str = ""):
+        """
+        Log a table metric to the run with the given name. Expects a dictionary
+        mapping column name to value.
+        """
+        self.experiment.log_table(name, value, description)
+
+    @rank_zero_only
     def finalize(self, status: str) -> None:
         """
-        https://docs.microsoft.com/en-us/python/api/azureml-core/azureml.core.run.run?view=azure-ml-py#flush-timeout-seconds-300-
+        Calls `.flush()` on the underlying Azure Machine Learning `Run`
+        object. This ensures all logs have been sent to Azure Machine Learning.
         """
         self.experiment.flush()
 
     @property
     def save_dir(self) -> Optional[str]:
-        pass
+        return None
 
     @property
     def name(self) -> str:
         """
-        https://docs.microsoft.com/en-us/python/api/azureml-core/azureml.core.run.run?view=azure-ml-py#id
+        Returns the ID of the underlying Azure Machine Learning `Run` object.
         """
         return self.experiment.id
 
     @property
     def version(self) -> str:
         r"""
-        Return
-
-        https://docs.microsoft.com/en-us/python/api/azureml-core/azureml.core.run.run?view=azure-ml-py#number
+        Returns the run number of the underlying Azure Machine Learning `Run`
+        object. Defaults to `0` if the run is offline.
         """
         if isinstance(self.experiment, AzureMlOfflineRun):
-            return '0'
+            return "0"
         else:
             return str(self.experiment.number)
--- tests/loggers/test_azureml.py
+++ tests/loggers/test_azureml.py
@@ -3,9 +3,21 @@ from unittest.mock import patch
 import pytest
 
 from pytorch_lightning import Trainer
-from pytorch_lightning.loggers import CometLogger
+from pytorch_lightning.loggers import AzureMlLogger
+
+
+def test_azureml_logger_exists():
+    """ Test launching two independent loggers. """
+    logger = AzureMlLogger()
+    logger2 = AzureMlLogger()
+    assert logger.experiment.id != logger2.experiment.id
 
 
 def test_azureml_logger_online():
     """ Test Azure ML logger online with mocks. """
     pass
+
+
+@patch('azureml.core.Run.get_context')
+def test_azureml_additional_methods(azureml):
+    logger = AzureMlLogger()
